2584640bd9014fb9de475991fe73110a3cc870c7
Dinchan.dev <dinhtruonghioo4@gmail.com>
2025-09-16 05:25:29 +0700

    Change script

--- build/src/config/mongodb.js
+++ build/src/config/mongodb.js
@@ -4,29 +4,29 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.GET_DB = exports.CONNECT_DB = void 0;
+exports.GET_DB = void 0;
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
-require("dotenv/config.js");
-var _mongodb = require("mongodb");
 // ta không dùng mongoose
-
-var MONGODB_URL = process.env.MONGODB_URL;
-var DB_NAME = process.env.DB_NAME;
+var env = require("./environment");
+var _require = require("mongodb"),
+  MongoClient = _require.MongoClient,
+  ServerApiVersion = _require.ServerApiVersion;
+var MONGODB_URL = env.MONGODB_URL;
+var DB_NAME = env.DB_NAME;
 
 // Khởi tạo đối tượng ban đầu
 var trelloDbInstance = null;
-
 // Khởi tạo một đối tượng để kết nối tới MongoDB
-var mongoClinetInstance = new _mongodb.MongoClient(MONGODB_URL, {
+var mongoClinetInstance = new MongoClient(MONGODB_URL, {
   serverApi: {
-    version: _mongodb.ServerApiVersion.v1,
+    version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true
   }
 });
 // Kết nối tới DB
-var CONNECT_DB = exports.CONNECT_DB = /*#__PURE__*/function () {
+var CONNECT_DB = /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])(/*#__PURE__*/_regenerator["default"].mark(function _callee() {
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) switch (_context.prev = _context.next) {
@@ -34,7 +34,7 @@ var CONNECT_DB = exports.CONNECT_DB = /*#__PURE__*/function () {
           _context.next = 2;
           return mongoClinetInstance.connect();
         case 2:
-          // kết nối thành công thì lấy DB theo tên và gán ngược nó lại vào biến 
+          // kết nối thành công thì lấy DB theo tên và gán ngược nó lại vào biến
           trelloDbInstance = mongoClinetInstance.db(DB_NAME);
         case 3:
         case "end":
@@ -49,4 +49,8 @@ var CONNECT_DB = exports.CONNECT_DB = /*#__PURE__*/function () {
 var GET_DB = exports.GET_DB = function GET_DB() {
   if (!trelloDbInstance) throw new Error("Must connect to database first!");
   return trelloDbInstance;
+};
+module.exports = {
+  CONNECT_DB: CONNECT_DB,
+  GET_DB: GET_DB
 };
--- build/src/server.js
+++ build/src/server.js
@@ -1,20 +1,46 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _express = _interopRequireDefault(require("express"));
-var _mongodb = require("./config/mongodb.js");
-require("dotenv/config.js");
+var express = require("express");
+var cors = require("cors");
+var _require = require("./config/cors.js"),
+  corsOptions = _require.corsOptions;
+var _require2 = require("./config/mongodb.js"),
+  CONNECT_DB = _require2.CONNECT_DB;
+var APIs_v1 = require("./routes/v1");
+var errorHandlingMiddleware = require("./middlewares/errorHandlingMiddleware.js");
+var cookieParser = require("cookie-parser");
+var env = require("./config/environment.js");
 var START_SERVER = function START_SERVER() {
-  var app = (0, _express["default"])();
-  var port = process.env.PORT;
-  app.get("/", function (req, res) {
-    res.send("Hello");
+  var app = express();
+  // Fix cái vụ Cache from disk của ExpressJS
+  // https://stackoverflow.com/a/53240717/8324172
+  app.use(function (req, res, next) {
+    res.set("Cache-Control", "no-store");
+    next();
   });
+
+  // Cấu hình cookie parser
+  app.use(cookieParser());
+
+  // Xu ly CORS
+  app.use(cors(corsOptions));
+
+  // Enable req.body json data
+  app.use(express.json());
+
+  //Use API v1
+  app.use("/v1", APIs_v1);
+
+  // Middleware xử lý lỗi tập trung
+  app.use(errorHandlingMiddleware);
+  var port = env.PORT;
   app.listen(port, function () {
     console.log("Hello Truong Nguyen, I'm running server at http://localhost:".concat(port));
   });
 };
-(0, _mongodb.CONNECT_DB)().then(function () {
+
+// Chỉ khi kết nối DB thành công thì mới start server
+CONNECT_DB().then(function () {
   console.log("Connected to MongoDB");
 }).then(function () {
   return START_SERVER();
