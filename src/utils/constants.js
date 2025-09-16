106c15f887dfa381c82b20c03e5655c2b61f7175
Dinchan.dev <dinhtruonghioo4@gmail.com>
2025-09-16 10:10:57 +0700

    add localhost to whitelist

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 // Những domain được phép truy cập đến tài nguyên của servers
-const WHITELIST_DOMAINS = ["https://trello-web-blue-phi.vercel.app"]
+const WHITELIST_DOMAINS = ["https://trello-web-blue-phi.vercel.app", "http://localhost:5173"]
 module.exports = { WHITELIST_DOMAINS }
